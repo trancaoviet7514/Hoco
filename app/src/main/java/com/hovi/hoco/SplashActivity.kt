ee369479e243a6b3ea9ca3e764570e8295387d0b
VietTC <viettc@vng.com.vn>
2020-12-12 10:08:09 +0700

    handle login flow

--- app/src/main/java/com/hovi/hoco/SplashActivity.kt
+++ app/src/main/java/com/hovi/hoco/SplashActivity.kt
@@ -14,7 +14,7 @@ class SplashActivity : AppCompatActivity() {
         if (FirebaseAuth.getInstance().currentUser != null) {
             startActivity(Intent(this, RemoteActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
         } else {
-            startActivity(Intent(this, LoginActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
+            startActivity(Intent(this, PreLoginActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
         }
     }
 }
---
+++ app/src/main/java/com/hovi/hoco/utils/FireBaseDataBaseUtils.kt
@@ -0,0 +1,89 @@
+package com.hovi.hoco.utils
+
+import android.os.Build
+import android.util.Log
+import android.view.View
+import androidx.annotation.RequiresApi
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import org.json.JSONObject
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.ObjectOutputStream
+import java.util.*
+import java.util.Base64.getEncoder
+import kotlin.collections.HashMap
+
+/**
+ * Created by VietTC on 05/12/2020.
+ */
+class FireBaseDataBaseUtils {
+
+    companion object {
+        fun writeDemo() {
+            val database = Firebase.database
+            val myRef = database.getReference("message/bbc/hi")
+myRef.setValue("abc")
+//            myRef.child("haha").setValue("Hello, World!")
+
+        }
+
+        fun signUp(userName: String, password: String, connectionString: String, callback: SignUpCallBack) {
+            val ref = Firebase.database.getReference(userName)
+
+            ref.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    ref.removeEventListener(this)
+                    val value = dataSnapshot.value
+                    if (value == null) {
+                        ref.child("password").setValue(password)
+                        ref.child("connectionString").setValue(connectionString)
+                        callback.onSuccess()
+                    } else {
+                        callback.onFail()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    callback.onFail()
+                }
+            })
+
+        }
+
+        fun login(userName: String, password: String, callback: SignUpCallBack) {
+            val ref = Firebase.database.getReference(userName)
+
+            ref.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    ref.removeEventListener(this)
+                    val value = dataSnapshot.value
+                    if (value != null && value is HashMap<*, *>) {
+                        if (value["password"] == password) {
+                            callback.onSuccess()
+                        } else {
+                            callback.onFail()
+                        }
+                    } else {
+                        callback.onFail()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    callback.onFail()
+                }
+            })
+
+        }
+    }
+
+    interface SignUpCallBack {
+        fun onSuccess()
+        fun onFail()
+    }
+}
