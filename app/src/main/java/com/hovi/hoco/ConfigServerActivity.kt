9fbca2c5e39d70a93bdc847149643a673644c7f9
VietTC <viettc@vng.com.vn>
2020-12-12 13:26:18 +0700

    handle connection string item

--- app/src/main/java/com/hovi/hoco/ConfigServerActivity.kt
+++ app/src/main/java/com/hovi/hoco/ConfigServerActivity.kt
@@ -4,14 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import com.hovi.hoco.databinding.ActivityConfigServerBinding
 
 class ConfigServerActivity : AppCompatActivity() {
 
     companion object {
-        val SERVER_IP = "SERVER_IP"
-        val SERVER_PORT = "SERVER_PORT"
+        const val CONNECTION_STRING = "CONNECTION_STRING"
     }
 
     lateinit var binding : ActivityConfigServerBinding
@@ -23,15 +22,12 @@ class ConfigServerActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbarMain)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val host = SharePreferenceUtils.getString(this, SERVER_IP, "tcp://plcshop.webhop.me")
-        val port = SharePreferenceUtils.getString(this, SERVER_PORT, "1883")
-        binding.txtIp.setText(host)
-        binding.txtPort.setText(port)
+        val host = SharePreferenceUtils.getString(this, CONNECTION_STRING, "tcp://plcshop.webhop.me:1883")
+        binding.txtConnectionString.setText(host)
 
-        binding.btnSaveServerConfig.setOnClickListener(View.OnClickListener {
-            SharePreferenceUtils.setString(this, SERVER_IP, binding.txtIp.text.toString())
-            SharePreferenceUtils.setString(this, SERVER_PORT, binding.txtPort.text.toString())
-            Toast.makeText(this@ConfigServerActivity, getString(R.string.str_save_success), Toast.LENGTH_LONG).show()
+        binding.btnSave.setOnClickListener(View.OnClickListener {
+            SharePreferenceUtils.setString(this, CONNECTION_STRING, binding.txtConnectionString.text.toString())
+            Snackbar.make(binding.root, "Cập nhật thành công", Snackbar.LENGTH_LONG).show()
         })
     }
 
--- app/src/main/java/com/hovi/hoco/SettingActivity.kt
+++ app/src/main/java/com/hovi/hoco/SettingActivity.kt
@@ -5,36 +5,33 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.auth.FirebaseAuth
 import com.hovi.hoco.databinding.ActivitySettingBinding
 
 class SettingActivity : AppCompatActivity() {
-    lateinit var binding: ActivitySettingBinding
+    lateinit var vb: ActivitySettingBinding
     var demoActivityTrigger = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySettingBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        vb = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(vb.root)
 
-        setSupportActionBar(binding.toolbarMain)
+        setSupportActionBar(vb.toolbarMain)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        vb.txtUserName.text = SharePreferenceUtils.getString(this, LoginActivity.CURRENT_USERNAME)
 
-        binding.itemConnect.setOnClickListener(View.OnClickListener {
+        vb.itemConnect.setOnClickListener(View.OnClickListener {
             startActivity(Intent(this, ConfigServerActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
         })
 
-        binding.itemClockApp.setOnClickListener(View.OnClickListener {  })
-        binding.itemDeviceSetting.setOnClickListener(View.OnClickListener {  })
-        binding.itemHelp.setOnClickListener(View.OnClickListener {  })
+        vb.itemClockApp.setOnClickListener(View.OnClickListener {  })
+        vb.itemDeviceSetting.setOnClickListener(View.OnClickListener {  })
+        vb.itemHelp.setOnClickListener(View.OnClickListener {  })
 
-        binding.itemSignOut.setOnClickListener(View.OnClickListener {
+        vb.itemSignOut.setOnClickListener(View.OnClickListener {
             AlertDialog.Builder(this)
                     .setMessage(getString(com.hovi.hoco.R.string.str_confirm_log_out))
                     .setPositiveButton(getString(com.hovi.hoco.R.string.str_yes)) { _, _ ->
@@ -46,7 +43,7 @@ class SettingActivity : AppCompatActivity() {
 
         })
 
-        binding.root.setOnClickListener(View.OnClickListener {
+        vb.root.setOnClickListener(View.OnClickListener {
             demoActivityTrigger++;
             if (demoActivityTrigger == 7) {
                 demoActivityTrigger = 0
